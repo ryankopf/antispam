aeca7f37245c024a4a58aca15987e1ff6b15b3d4
Ryan Kopf <ryankopf@users.noreply.github.com>
2021-01-31 10:07:54 -0600

    Updating.

--- app/controllers/antispam/application_controller.rb
+++ app/controllers/antispam/application_controller.rb
@@ -1,4 +1,11 @@
 module Antispam
   class ApplicationController < ActionController::Base
+    def must_be_admin
+      begin
+        render plain: '' unless is_admin?
+      rescue
+        render plain: ''
+      end
+    end
   end
 end
--- app/controllers/antispam/blocks_controller.rb
+++ app/controllers/antispam/blocks_controller.rb
@@ -3,7 +3,7 @@ require_dependency "antispam/application_controller"
 module Antispam
   class BlocksController < ApplicationController
     before_action :must_be_admin
-    before_action :set_block, only: [:show, :edit, :update, :destroy]
+    before_action :set_block, only: [:show]
 
     # GET /blocks
     def index
@@ -14,41 +14,6 @@ module Antispam
     def show
     end
 
-    # # GET /blocks/new
-    # def new
-    #   @block = Block.new
-    # end
-    #
-    # # GET /blocks/1/edit
-    # def edit
-    # end
-    #
-    # # POST /blocks
-    # def create
-    #   @block = Block.new(block_params)
-    #
-    #   if @block.save
-    #     redirect_to @block, notice: 'Block was successfully created.'
-    #   else
-    #     render :new
-    #   end
-    # end
-    #
-    # # PATCH/PUT /blocks/1
-    # def update
-    #   if @block.update(block_params)
-    #     redirect_to @block, notice: 'Block was successfully updated.'
-    #   else
-    #     render :edit
-    #   end
-    # end
-    #
-    # # DELETE /blocks/1
-    # def destroy
-    #   @block.destroy
-    #   redirect_to blocks_url, notice: 'Block was successfully destroyed.'
-    # end
-
     private
       # Use callbacks to share common setup or constraints between actions.
       def set_block
@@ -58,13 +23,6 @@ module Antispam
       # Only allow a list of trusted parameters through.
       def block_params
         params.require(:block).permit(:ip, :provider, :controllername, :actionname)
-      end
-      def must_be_admin
-        begin
-          return false unless is_admin?
-        rescue
-          return false
-        end
       end
   end
 end
