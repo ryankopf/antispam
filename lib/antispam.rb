436dcddcdaf1f84f5a3508e70c27e85b32ade4de
ryankopf <ryan@ryankopf.com>
2021-02-06 16:38:13 -0600

    Minor updates.

--- lib/antispam.rb
+++ lib/antispam.rb
@@ -7,5 +7,7 @@ module Antispam
   ActiveSupport.on_load(:action_controller) do
     # self refers to ActionController::Base here
     self.include Antispam::Tools
+    # Would the below be a better (clearer? more compatible?) way to do this?
+    # ActionController::Base.send(:include, Antispam::Tools)
   end
 end
