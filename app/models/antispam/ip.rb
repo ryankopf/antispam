cc8d54784bc12d758973bae6743503c9e422a7c5
ryankopf <git@ryankopf.com>
2023-07-01 15:57:42 -0500

    Updates.

--- app/models/antispam/ip.rb
+++ app/models/antispam/ip.rb
@@ -1,7 +1,12 @@
 module Antispam
   class Ip < ApplicationRecord
+    before_initialize :set_default_expires_at
     def expired?
       self.expires_at < Time.now
+    end
+    private
+    def set_default_expires_at
+      self.expires_at ||= 24.hours.from_now
     end
   end
 end
