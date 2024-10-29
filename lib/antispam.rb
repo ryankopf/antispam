75feee79bb1f7875abd8cc92ca734ae5121e5b33
Ryan Kopf <github@ryankopf.com>
2024-10-28 22:28:13 -0500

    Base.

--- lib/antispam.rb
+++ lib/antispam.rb
@@ -8,14 +8,6 @@ require "antispam/results"
 
 module Antispam
   ActiveSupport.on_load(:action_controller_base) do
-    # Include Antispam::Tools into the application's ApplicationController
-    # Use ::ApplicationController to reference the top-level class
-    if defined?(::ApplicationController)
-      ::ApplicationController.include Antispam::Tools
-    else
-      Rails.application.config.to_prepare do
-        ::ApplicationController.include Antispam::Tools
-      end
-    end
+    ActionController::Base.include Antispam::Tools
   end
 end
--- lib/antispam/version.rb
+++ lib/antispam/version.rb
@@ -1,3 +1,3 @@
 module Antispam
-  VERSION = '0.2.8'
+  VERSION = '0.2.9'
 end
