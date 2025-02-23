50b31b67ea00d3c9a89d31a37966d48d1c2c912d
Ryan Kopf <github@ryankopf.com>
2025-02-23 07:31:53 -0600

    X

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Antispam::Engine.routes.draw do
   resources :clears
   resources :blocks
   resources :challenges
+  resources :signups
   root to: 'ips#index'
   get 'validate', to: 'validate#index'
 end
