f9c7d6e55fce46f01b253bf6824f595ac1ef6acb
Ryan Kopf <github@ryankopf.com>
2025-02-24 18:27:23 -0600

    Score

--- app/models/antispam/signup.rb
+++ app/models/antispam/signup.rb
@@ -15,13 +15,13 @@ module Antispam
       spamscore += 10 if self.number_from_this_ip > 5
       spamscore += 10 if self.number_from_this_ip > 10
       spamscore += 10 if Iplocator.countries_suspected_of_spam.include?(self.country_code)
-      spamscore -= 10 if self.number_from_this_ip < 2
+      spamscore -= 5 if self.number_from_this_ip < 2
       spamscore -= 10 if Iplocator.trusted_countries.include?(self.country_code)
       spamscore = [[spamscore, 0].max, 100].min # Clamp to a value between 0 and 100
       spamscore
     end
     def safe?
-      self.spamscore < 50
+      self.spamscore < 40
     end
   end
 end
